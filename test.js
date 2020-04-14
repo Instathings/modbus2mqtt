25e0959c6b4b66b449661ccce447acc29f51a096
Claudio Petrini <claudio.petrini@instathings.io>
2020-04-14 09:51:22 +0200

    reading new format

--- test.js
+++ test.js
@@ -5,21 +5,21 @@ async function test() {
   await client.subscribe('modbus2mqtt/bridge/log');
   client.on('message', (topic, message) => {
     console.log(topic, message.toString());
-  })
-  // console.log('connected');
-  // const payload = {
-  //   id: 'xfwog',
-  //   modbus_id: 1,
-  //   baud_rate: 9600,
-  //   interval: 10000,
-  //   model: 'xy-md02',
-  // };
-  // await client.publish('modbus2mqtt/configure/set', JSON.stringify(payload));
-  // console.log('PUBLISHED');
+  });
+  console.log('connected');
   const payload = {
     id: 'xfwog',
+    modbus_id: 1,
+    baud_rate: 9600,
+    interval: 10000,
+    model: 'xy-md02',
   };
-  await client.publish('modbus2mqtt/bridge/config/force_remove', JSON.stringify(payload));
+  await client.publish('modbus2mqtt/configure/set', JSON.stringify(payload));
+  console.log('PUBLISHED');
+  // const payload = {
+  //   id: 'xfwog',
+  // };
+  // await client.publish('modbus2mqtt/bridge/config/force_remove', JSON.stringify(payload));
 }
 
 test();
